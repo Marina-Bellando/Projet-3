32e5f7bd3908965a574d7360722231a187fb99af
Marina Bellando <eljhawood@hotmail.fr>
2024-03-23 22:44:45 +0100

    Partie 3.1 terminée

--- FrontEnd/Modale.js
+++ FrontEnd/Modale.js
@@ -17,17 +17,17 @@ const focusableSelector = "button, a, input, textarea"
 let focusables = []
 
 const openModal = function (e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    modal = document.querySelector(e.target.getAttribute("href"))
-    focusables = Array.from(modal.querySelectorAll(focusableSelector))
-    focusables[0].focus
-    modal.style.display = null
-    modal.removeAttribute("aria-hidden")
-    modal.setAttribute("aria-modal", "true")
-    modal.addEventListener("click", closeModal)
-    modal.querySelector(".js-modal-close").addEventListener("click", closeModal)
-    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
+    modal = document.querySelector(e.target.getAttribute("href"));
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    focusables[0].focus();
+    modal.style.display = null;
+    modal.removeAttribute("aria-hidden");
+    modal.setAttribute("aria-modal", "true");
+    modal.addEventListener("click", closeModal);
+    modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
+    modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation);
 }
 const closeModal = function(e) {
     if(modal === null) return
@@ -81,13 +81,15 @@ function creerProjet2(work) {
     // Créer les éléments pour chaque partie
         const figure = document.createElement("figure");
         const img = document.createElement("img");
-        const deleteIcon = document.createElement("span");
+        const deleteIcon = document.createElement("button");
     // Définit les éléments
         img.src = work.imageUrl;
         deleteIcon.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
     // Ajout des classes pour la grille
         figure.classList.add("gallery-photo");
         deleteIcon.classList.add("delete-icon");
+    // Ajout de l'attribut data-work-id au bouton de suppression
+        deleteIcon.dataset.workId = work.id;
     // Ajout des éléments
         figure.appendChild(img);
         figure.appendChild(deleteIcon);
@@ -105,3 +107,41 @@ async function afficherProjet2() {
 }
 
 afficherProjet2();
+
+    // Fonction pour ouvrir la 1ère modale
+function afficherModal1(e) {
+    e.preventDefault();
+    // Affiche uniquement la modal-galerie-photo
+    document.querySelector(".modal-galerie-photo").style.display = "flex";
+    document.getElementById("modal-ajout-photo").style.display = "none";
+}
+    // Quand on appui sur le bouton modifier
+document.querySelectorAll(".js-modal").forEach(a => {
+    a.addEventListener("click", afficherModal1);
+});
+
+    // Fonction pour ouvrir la 2ème modale
+function afficherModal2(e) {
+    e.preventDefault();
+    // Affiche uniquement la modal-ajout-photo
+    document.querySelector(".modal-galerie-photo").style.display = "none";
+    document.getElementById("modal-ajout-photo").style.display = "flex";
+}
+    //Quand on appui sur le bouton ajouter une photo
+document.querySelectorAll(".btn-AjoutPhoto").forEach(a => {
+    a.addEventListener("click", afficherModal2);
+});
+    // Pour fermer la 2ème modale
+document.querySelectorAll("#modal-ajout-photo .js-modal-close").forEach(a => {
+    a.addEventListener("click", closeModal);
+});
+
+    // Sélectionne le bouton de retour
+const retourneModalBtn = document.querySelector(".retourner-modal1");
+    // Quand on appui sur le bouton retour, affiche Modale1
+retourneModalBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("modal-ajout-photo").style.display = "none";
+    document.querySelector(".modal-galerie-photo").style.display = "flex";
+});
+
